aa263ed6fac445ee882ea87f2e576d181377f2da
Thomas <Thomas@DESKTOP-A4MAJIF.home>
2020-11-13 18:00:54 +0100

    stock modif

--- src/classes/cuisine/Stock.java
+++ src/classes/cuisine/Stock.java
@@ -5,8 +5,8 @@ import classes.cuisine.materiel.Assiette;
 public class Stock {
 	private Assiette[] stockassiette;
 	
-	public Stock() {
-		
+	public Stock(int  taille_du_stock) {
+		this.stockassiette = new Assiette[taille_du_stock];
 	}
 	private Assiette prendreAssiette() {
 		return stockassiette[stockassiette.length+1];
