c7b17112cc933b3146899a6e32820bd4f059841e
Thomas <Thomas@DESKTOP-A4MAJIF.home>
2020-11-13 17:30:56 +0100

    changement GardeManger nommage

--- src/classes/cuisine/GardeManger.java
+++ src/classes/cuisine/GardeManger.java
@@ -1,4 +1,14 @@
 package classes.cuisine;
 
+import classes.cuisine.Ingredient.Nom;
+
 public class GardeManger {
+	
+	Ingredient[] gardemanger;
+	
+	public Ingredient saisirUnIngredient(Ingredient.Nom nom) {
+		Ingredient ingredient = new Ingredient(nom);
+		return ingredient;
+	}
+	
 }
