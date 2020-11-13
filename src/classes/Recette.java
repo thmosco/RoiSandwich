aa61492546662179cd054bf58d46c8b6e9796b33
Maia <maia.dasilva.12@gmail.com>
2020-11-13 17:13:17 +0100

    Update Recette.java

--- src/classes/Recette.java
+++ src/classes/Recette.java
@@ -1,8 +1,20 @@
 package classes;
+import classes.cuisine.Ingredient;
+import java.util.HashMap;
 
 public class Recette {
 
-    public enum NomRecette {
+    public enum NomsRecettes {
 
+    }
+
+    private NomsRecettes nom ;
+    private HashMap<Ingredient, Integer> ingredients ;
+
+    public Recette (NomsRecettes nom) {
+        this.nom = nom ;
+        switch (nom) {
+            case
+        }
     }
 }
