b4459c16c93026a9422ac7e046025d059efa1cea
Thomas <Thomas@DESKTOP-A4MAJIF.home>
2020-11-13 17:29:26 +0100

    modif Ingredient getter

--- src/classes/cuisine/Ingredient.java
+++ src/classes/cuisine/Ingredient.java
@@ -2,18 +2,30 @@ package classes.cuisine;
 
 public class Ingredient {
 
-    private Nom nom;
-    //private int quantite;//
-    private boolean transformer;
-    private Etat etat;
+	private Nom nom;
+	// private int quantite;//
+	private boolean transformer;
+	private Etat etat;
 
-    public enum Etat{CRU, CUIT, BRULE}
+	public enum Etat {
+		CRU, CUIT, BRULE
+	}
 
-    public enum Nom{POMME_DE_TERRE, SALADE, OIGNON, PAIN, FROMAGE, STEAK_DE_SOJA, STEAK_DE_POULET, STEAK_DE_BOEUF}
+	public enum Nom {
+		POMME_DE_TERRE, SALADE, OIGNON, PAIN, FROMAGE, STEAK_DE_SOJA, STEAK_DE_POULET, STEAK_DE_BOEUF, TOMATE
+	}
 
+	public Ingredient(Nom nom) {
+		this.nom = nom;
+		this.etat = Etat.CRU;
+	}
 
-    public Ingredient(Nom nom, Etat etat) {
-        this.nom = nom;
-        this.etat = Etat.CRU;
+	public Etat getEtat() {
+    	return this.etat;
     }
+
+	public boolean getTransformer() {
+    	return transformer;
+    }
+
 }
