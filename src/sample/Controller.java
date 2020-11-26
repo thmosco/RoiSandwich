926d31216b5563d9bc6614d49bc74315cc84c2c6
Thomas <Thomas@DESKTOP-A4MAJIF.home>
2020-11-26 14:30:22 +0100

    java_fx niveau1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import classes.cuisine.Ingredient;
+import classes.cuisine.Ingredient.Nom;
+
 //test
-public class Controller {
+public class Controller implements Initializable {
+	@FXML
+	Button buttonSalade;
+	
+	  public void salade(ActionEvent buttonSalade) {
+      	Ingredient salade = new Ingredient(Nom.SALADE);
+      	System.out.print(salade.getTransformer());
+      }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,28 @@
 package sample;
 
+
+import classes.cuisine.Ingredient;
+import classes.cuisine.Ingredient.Nom;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    	Parent root = FXMLLoader.load(getClass().getResource("niveau1.fxml"));
+		primaryStage.setTitle("RoiSandwich");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
-    }
+        }
+
 }
