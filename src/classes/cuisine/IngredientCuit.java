cc307050e7a471a83a9f42cd9b3a0ff9d91d02b4
Maia <maia.dasilva.12@gmail.com>
2020-11-30 19:19:43 +0100

    Update IngredientCuit.java

--- src/classes/cuisine/IngredientCuit.java
+++ src/classes/cuisine/IngredientCuit.java
@@ -5,20 +5,47 @@
  */
 package classes.cuisine;
 
+/**
+ * Commentaire de documentation de la classe IngrédientCuit
+ * @version 1.0
+ * @author Thomas MOSCONI
+ *
+ * @version 2.0
+ * @author Maia DA SILVA
+ */
 public class IngredientCuit extends Ingredient {
 
-	private int tempsCuisson = 10000;
-	
+	/**
+	 * Représente le temps de cuisson optimal de l'aliment
+	 */
+	private int tmpsCuisson ;
+
+
+	/**
+	 * Constructeur avec temps de cuisson définit par défaut
+	 * @param nom
+	 */
 	public IngredientCuit(Nom nom) {
 		super(nom);
+		this.tmpsCuisson = 10000 ;
 	}
 
-	public IngredientCuit(Nom nom, int temps_de_cuisson) {
+	/**
+	 * Constructeur avec précision du temps de cuisson
+	 * @param nom
+	 * @param tmpsCuisson
+	 */
+	public IngredientCuit(Nom nom, int tmpsCuisson) {
 		super(nom);
-		this.tempsCuisson = temps_de_cuisson;
+		this.tmpsCuisson = tmpsCuisson ;
 	}
 
+	// Getteur
+
+	/**
+	 * @return le temps de cuisson optimale d'un ingrédient cuisable
+	 */
 	public int getTempsCuisson() {
-		return tempsCuisson;
+		return tmpsCuisson;
 	}
 }
